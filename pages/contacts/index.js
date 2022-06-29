db9c638c67a3b993216bc7f6372553d983f5cf28
Emil <emil.h098@gmail.com>
2022-06-29 22:16:22 +0400

    fetching-data

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -2,11 +2,14 @@ import Head from "next/head";
 import Link from "next/link";
 import Heading from "../../components/Heading";
 
+
+// 
 export const getStaticProps = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
 
   if (!data) {
+    // es dupqum kveradardzni 404 errore
     return {
       notFound: true,
     }
@@ -21,13 +24,13 @@ const Contacts = ({ contacts }) => {
   return (
     <>
       <Head>
-        <title>Contacts</title>
+        <title>{Contacts}</title>
       </Head>
       <Heading text="Contacts list:" />
       <ul>
         {contacts && contacts.map(({ id, name }) => (
           <li key={id}>
-            <Link href={`/contacts/${id}`}>{name}</Link>
+            {/* <Link href={`/contacts/${id}`}>{name}</Link> */}
           </li>
         ))}
       </ul>
