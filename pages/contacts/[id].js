dda75609b19043c49d404ab48887bff6848d2b03
Emil <emil.h098@gmail.com>
2022-06-29 23:11:01 +0400

    ssg-ssr

--- pages/contacts/[id].js
+++ pages/contacts/[id].js
@@ -1,6 +1,22 @@
 import Head from "next/head";
 import ContactInfo from "../../components/ContactInfo";
 
+
+export const getServerSideProps = async (context) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  if (!data) {
+    // es dupqum kveradardzni 404 errore
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { contacts: data },
+  }
+};
 const Contact = ({ contact }) => (
   <>
     <Head>
